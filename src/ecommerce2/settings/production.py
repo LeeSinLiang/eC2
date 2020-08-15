2887e4a623e6731d33b66427cf407f791424e788
LeeSinLiang <mangojamlee@gmail.com>
2020-08-15 17:58:23 +0800

    intial commit

--- src/ecommerce2/settings/production.py
+++ src/ecommerce2/settings/production.py
@@ -21,150 +21,149 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 from django.conf import settings
 import django_heroku
-if not settings.DEBUG:
-	import os
+import os
 
-	BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-	#root of project
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#root of project
 
-	# Quick-start development settings - unsuitable for production
-	# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-	# SECURITY WARNING: keep the secret key used in production secret!
-	SECRET_KEY = 'csqwlmc8s55o($rt6ozh7u+ui9zb-et00w$d90j8$^!nvj41_r'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'csqwlmc8s55o($rt6ozh7u+ui9zb-et00w$d90j8$^!nvj41_r'
 
-	# SECURITY WARNING: don't run with debug turned on in production!
-	DEBUG = False
-
-	
-
-	
-	
-
-	EMAIL_HOST = 'smtp.gmail.com'
-	EMAIL_HOST_USER = 'ostuff114@gmail.com'
-	EMAIL_HOST_PASSWORD = 'da@ostuffDa114'
-	EMAIL_PORT = 587
-	EMAIL_USE_TLS = True
-
-	''' 
-	If using gmail, you will need to
-	unlock Captcha to enable Django 
-	to  send for you:
-	https://accounts.google.com/displayunlockcaptcha
-	'''
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
 
 
-	# Application definition
-
-	INSTALLED_APPS = (
-	    #django app
-	    'django.contrib.admin',
-	    'django.contrib.auth',
-	    'django.contrib.contenttypes',
-	    'django.contrib.sessions',
-	    'django.contrib.sites',
-	    'django.contrib.messages',
-	    'django.contrib.staticfiles',
-	    #third party apps
-	    'crispy_forms',
-	    'registration',
-	    #my apps
-	    'newsletter',
-	)
-
-	MIDDLEWARE_CLASSES = (
-	    'django.contrib.sessions.middleware.SessionMiddleware',
-	    'django.middleware.common.CommonMiddleware',
-	    'django.middleware.csrf.CsrfViewMiddleware',
-	    'django.contrib.auth.middleware.AuthenticationMiddleware',
-	    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-	    'django.contrib.messages.middleware.MessageMiddleware',
-	    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	    'django.middleware.security.SecurityMiddleware',
-	)
-
-	ROOT_URLCONF = 'ecommerce2.urls'
-
-	TEMPLATES = [
-	    {
-	        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-	        'DIRS': [os.path.join(BASE_DIR, "templates")],
-	        'APP_DIRS': True,
-	        'OPTIONS': {
-	            'context_processors': [
-	                'django.template.context_processors.debug',
-	                'django.template.context_processors.request',
-	                'django.contrib.auth.context_processors.auth',
-	                'django.contrib.messages.context_processors.messages',
-	            ],
-	        },
-	    },
-	]
-
-	WSGI_APPLICATION = 'ecommerce2.wsgi.application'
 
 
-	# Database
-	# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-	
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ostuff114@gmail.com'
+EMAIL_HOST_PASSWORD = 'da@ostuffDa114'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-	DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+''' 
+If using gmail, you will need to
+unlock Captcha to enable Django 
+to  send for you:
+https://accounts.google.com/displayunlockcaptcha
+'''
+
+
+
+# Application definition
+
+INSTALLED_APPS = (
+	#django app
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	#third party apps
+	'crispy_forms',
+	'registration',
+	#my apps
+	'newsletter',
+)
+
+MIDDLEWARE_CLASSES = (
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'ecommerce2.urls'
+
+TEMPLATES = [
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, "templates")],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
+]
+
+WSGI_APPLICATION = 'ecommerce2.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+
+
+DATABASES = {
+'default': {
+	'ENGINE': 'django.db.backends.sqlite3',
+	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 
-	# Internationalization
-	# https://docs.djangoproject.com/en/1.8/topics/i18n/
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-	LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-	TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-	USE_I18N = True
+USE_I18N = True
 
-	USE_L10N = True
+USE_L10N = True
 
-	USE_TZ = True
-
-
-	# Static files (CSS, JavaScript, Images)
-	# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-	STATICFILES_DIRS = [
-		os.path.join(BASE_DIR, "staticfiles"),
-	]
-
-	AWS_ACCESS_KEY_ID = 'AKIAJISFDAMIW7HTBKZA'
-	AWS_SECRET_ACCESS_KEY = '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'
-	AWS_STORAGE_BUCKET_NAME = 'ec2-assets-888'
-	AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-	AWS_S3_OBJECT_PARAMETERS = {
-		'CacheControl': 'max-age=86400',
-	}
-	AWS_LOCATION = 'static'
-
-	STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-	STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-	DEFAULT_FILE_STORAGE = 'ecommerce2.settings.storage_backends.MediaStorage'
+USE_TZ = True
 
 
-	#Crispy FORM TAGs SETTINGS
-	CRISPY_TEMPLATE_PACK = 'bootstrap3'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "staticfiles"),
+]
+
+AWS_ACCESS_KEY_ID = 'AKIAJISFDAMIW7HTBKZA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'ec2-assets-888'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+	'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'ecommerce2.settings.storage_backends.MediaStorage'
 
 
-	#DJANGO REGISTRATION REDUX SETTINGS
-	ACCOUNT_ACTIVATION_DAYS = 7
-	REGISTRATION_AUTO_LOGIN = True
-	SITE_ID = 1
-	LOGIN_REDIRECT_URL = '/'
+#Crispy FORM TAGs SETTINGS
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-	django_heroku.settings(locals())
+
+#DJANGO REGISTRATION REDUX SETTINGS
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
 
 
 
